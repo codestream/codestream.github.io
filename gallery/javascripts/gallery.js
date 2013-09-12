06f3edb828c819d23470dcec1428ce305dc38304
Viacheslav Okolitiy <justforgit@gmail.com>
2013-09-12 03:27:44 +0300

    0.1a

--- gallery/javascripts/gallery.js
+++ gallery/javascripts/gallery.js
@@ -5,7 +5,7 @@ window.onload = function(){
      * Returns an array with full image source
      */
     var bigImages = [
-        "images/full/img/303.png",
+        "images/full/303.png",
         "images/full/307.png",
         "images/full/313.png",
         "images/full/300.png",
@@ -61,6 +61,7 @@ window.onload = function(){
         this.currentPreview = "current";
         this.selectedElement = document.querySelectorAll("li > .preview");
         this.fullImage = document.getElementById("fullImage");
+        this.slideBack = {};
     }
 
     Gallery.prototype = {
@@ -73,6 +74,25 @@ window.onload = function(){
                     this.fullImage.src = bigImages[i];
                     if (i == this.selectedElement.length - 1) {
                         clearInterval(interval);
+                        interval = setInterval(function(){
+                            g.slideBackward();
+                        },5000);
+                    }
+                }
+            }
+        },
+
+        slideBackward : function(){
+            for(var i = 0; i < this.selectedElement.length; i++){
+                if(hasClass(this.selectedElement[i], this.currentPreview)){
+                    removeClass(this.selectedElement[i], this.currentPreview);
+                    addClass(this.selectedElement[i -= 1], this.currentPreview);
+                    this.fullImage.src = bigImages[i];
+                    if(i == 0){
+                        clearInterval(/*this.slideBack*/interval);
+                        interval = setInterval(function(){
+                            g.slideGallery()
+                        },5000);
                     }
                 }
             }
@@ -84,20 +104,43 @@ window.onload = function(){
                 for(var i = 0; i < gallery.selectedElement.length; i++){
                     var target = getEventTarget(event);
                     if(target === gallery.selectedElement[i]){
-                        clearInterval(interval);
+                        clearInterval(interval/* | this.slideBack*/);
                     }
                 }
             };
 
             document.addEventListener("click", listener, false);
+        },
+
+        scrollFromKeyboard : function(){
+            var gallery = new Gallery();
+            var count = 0;
+            var keyDownListener = function(event){
+                count++;
+                if(event.keyCode == 39){
+                    clearInterval(interval);
+                    gallery.slideGallery();
+                    //todo fix typeerror
+                }
+
+                if(event.keyCode == 37){
+                    clearInterval(interval);
+                    gallery.slideBackward();
+                    //todo fix typerrror
+                }
+            };
+
+            document.addEventListener('keydown', keyDownListener, false);
         }
     };
 
     var g = new Gallery();
 
-    var interval = setInterval(function () {
+    var interval = window.setInterval(function () {
         g.slideGallery();
     }, 5000);
 
     g.galleryItemClick();
+
+    g.scrollFromKeyboard();
 };
