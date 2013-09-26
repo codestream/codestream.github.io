101578276d436dc9ba968d07a208b46d2ddf2ccf
Viacheslav Okolitiy <justforgit@gmail.com>
2013-09-26 21:44:20 +0300

    Fixed

--- ajax/rozetkaParser.js
+++ ajax/rozetkaParser.js
@@ -1,3 +1,20 @@
+/***************************************************************************************************
+ * Парсинг розетки версия 2.0
+ * Пофикшено:
+ * 1) Дубликаты товаров(проверял JSON.stringify(data) и поиском дубликатов в
+ * JSON объекте)
+ * 2) Парсинг теперь идет адекватно, т.е парсятся все страницы каталога товаров
+ * с флешками
+ * 3)Поправил регулярки, так как не на всех страницах розетки регулярки отрабатывают как и на первой
+ * 4)Вместо JSON объекта использую массив
+ * 5)Добавлены проверки:
+ * - длина контейнера
+ * - наличие нод
+ * 6)Вместо document решил в body записывать респонс и парсить body, так как в случае с document
+ * возможны дубликаты товаров
+ *
+ ****************************************************************************************************/
+
 'use strict';
 
 (function(callback){
@@ -25,48 +42,73 @@
         //берем селектор контейнера
         var containers = domNode.querySelectorAll('.gtile-i-wrap');
         //если длина контейнера не 0
-        var parseContainerData = function(container){
-            var uaPriceNode = container.querySelector('.g-price-uah');
-            var usdPriceNode = container.querySelector('.g-price-usd');
-            var productNameNode = container.querySelector('.gtile-i-title');
-            var urlNode = container.querySelector('.gtile-i-title a');
-            return {
-                parseUAPrices: function(){
-                    return parseFloat(uaPriceNode.innerHTML.match(/\d+(.\d+)?/g)[0]).toFixed(2);
-                },
+        if(containers.length !== 0){
+            var parseContainerData = function(container){
+                var uaPriceNode = container.querySelector('.g-price-uah');
+                var usdPriceNode = container.querySelector('.g-price-usd');
+                var productNameNode = container.querySelector('.gtile-i-title');
+                var urlNode = container.querySelector('.gtile-i-title a');
+                var memoryNode = container.querySelectorAll('.gtile-short-detail li')[0];
+                return {
+                    parseUAPrices: function(){
+                        if(uaPriceNode){
+                            var uaPrice = parseFloat(uaPriceNode.innerHTML.match(/(\d+(.\d+)?)/g)[0]).toFixed(2);
+                        } else {
+                            uaPrice = 0;
+                        }
 
-                parseUSDPrices: function(){
-                    return parseFloat(usdPriceNode.innerHTML.match(/\d+(.\d+)?/g)[0]).toFixed(2);
-                },
+                        return uaPrice;
+                    },
 
-                parseProductNames: function(){
-                    return productNameNode.innerHTML.match(/\S+/g).join(' ');
-                },
+                    parseUSDPrices: function(){
+                        if(usdPriceNode){
+                            var usdPrice = parseFloat(usdPriceNode.innerHTML.match(/(\d+(.\d+)?)/g)[0]).toFixed(2);
+                        } else {
+                            usdPrice = 0;
+                        }
 
-                parseProductURLS: function(){
-                    return urlNode.href;
-                },
+                        return usdPrice;
+                    },
 
-                parseMemoryVolume: function(){
-                    return productNameNode.innerHTML.match(/(?:\s)\d+(\s+)?GB(?:\s)/g)[0].replace(/\s/g, '');
-                },
+                    parseProductNames: function(){
+                        if(productNameNode){
+                            var productName = productNameNode.textContent.match(/\S+/g).join(' ');
+                        }
 
-                buildProductData: function(){
-                    var productObject = {};
-                    productObject.ua_price = this.parseUAPrices();
-                    productObject.usd_price = this.parseUSDPrices();
-                    productObject.name = this.parseProductNames();
-                    productObject.url = this.parseProductURLS();
-                    productObject.memory = this.parseMemoryVolume();
+                        return productName;
+                    },
 
-                    return productObject;
+                    parseProductURLS: function(){
+                        return urlNode.href;
+                    },
+
+                    parseMemoryVolume: function(){
+                        if(memoryNode){
+                            var memory = memoryNode.innerHTML.match(/\d+/g);
+                            var convertToInt = parseInt(memory);
+                        }
+
+                        return convertToInt;
+                    },
+
+                    buildProductData: function(){
+                        var productObject = {};
+                        productObject.ua_price = parseFloat(this.parseUAPrices());
+                        productObject.usd_price = parseFloat(this.parseUSDPrices());
+                        productObject.name = this.parseProductNames();
+                        productObject.url = this.parseProductURLS();
+                        productObject.memory = this.parseMemoryVolume();
+
+                        return productObject;
+                    }
                 }
-            }
-        };
+            };
+        }
 
         for(var i = 0; i < containers.length; i++){
             products.push(parseContainerData(containers[i]).buildProductData());
         }
+
 
         return products;
     };
@@ -78,21 +120,43 @@
             if (request.status === 200) {
                 var body = document.createElement('body');
                 body.innerHTML = request.responseText.match(/<body>[\s\S]*<\/body>/gim)[0];
-                callback(parseData(body));
+                parseData(body);
                 var pager = body.querySelectorAll('.goods-pages-list li');
                 totalPages = parseInt(pager[pager.length - 1].getAttribute('id').match(/\d+/g)[0]);
+                loadAllProducts();
             }
         }
     };
 
     request.send(null);
+
+    function loadAllProducts(){
+        if(currentPage < totalPages){
+            currentPage++;
+        } else {
+            return;
+        }
+
+        var currentUrl = url + 'page=' + currentPage + '/';
+        request.open('get', currentUrl, true);
+        request.onreadystatechange = function(){
+            if(request.readyState === 4){
+                if(request.status === 200){
+                    var body = document.createElement('body');
+                    body.innerHTML = request.responseText.match(/<body>[\s\S]*<\/body>/gim)[0];
+                    parseData(body);
+                    if(currentPage == totalPages){
+                        callback(products);
+                    }
+                }
+                loadAllProducts();
+            }
+        };
+        request.send(null);
+    }
 })(showData);
 
 function showData(data){
    console.log(data);
-    console.log(data.length);
+   console.log(data.length);
 }
-
-window.onerror = function(error){
-    console.log(error);
-};
