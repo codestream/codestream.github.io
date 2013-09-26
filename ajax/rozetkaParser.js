36d9dfa3168950083d906a4730f72d5f2f6c8e5e
Viacheslav Okolitiy <justforgit@gmail.com>
2013-09-26 15:43:01 +0300

    Removed invalid code

--- ajax/rozetkaParser.js
+++ ajax/rozetkaParser.js
@@ -25,51 +25,47 @@
         //берем селектор контейнера
         var containers = domNode.querySelectorAll('.gtile-i-wrap');
         //если длина контейнера не 0
-        if(containers.length !== 0){
-            //анонимная функция для парсинга данных и создания объекта с ними
-            //для выдачи в консоль
-            var parseContainerData = function(container){
-                var uaPriceNode = container.querySelector('.g-price-uah');
-                var usdPriceNode = container.querySelector('.g-price-usd');
-                var productNameNode = container.querySelector('.gtile-i-title');
-                var urlNode = container.querySelector('.gtile-i-title a');
-                return {
-                    parseUAPrices: function(){
-                        return parseFloat(uaPriceNode.textContent.match(/\d+(.\d+)?/g)[0]).toFixed(2);
-                    },
+        var parseContainerData = function(container){
+            var uaPriceNode = container.querySelector('.g-price-uah');
+            var usdPriceNode = container.querySelector('.g-price-usd');
+            var productNameNode = container.querySelector('.gtile-i-title');
+            var urlNode = container.querySelector('.gtile-i-title a');
+            return {
+                parseUAPrices: function(){
+                    return parseFloat(uaPriceNode.innerHTML.match(/\d+(.\d+)?/g)[0]).toFixed(2);
+                },
 
-                    parseUSDPrices: function(){
-                        return parseFloat(usdPriceNode.textContent.match(/\d+(.\d+)?/g)[0]).toFixed(2);
-                    },
+                parseUSDPrices: function(){
+                    return parseFloat(usdPriceNode.innerHTML.match(/\d+(.\d+)?/g)[0]).toFixed(2);
+                },
 
-                    parseProductNames: function(){
-                        return productNameNode.textContent.match(/\S+/g).join(' ');
-                    },
+                parseProductNames: function(){
+                    return productNameNode.innerHTML.match(/\S+/g).join(' ');
+                },
 
-                    parseProductURLS: function(){
-                        return urlNode.href;
-                    },
+                parseProductURLS: function(){
+                    return urlNode.href;
+                },
 
-                    parseMemoryVolume: function(){
-                        return productNameNode.textContent.match(/(?:\s)\d+(\s+)?GB(?:\s)/g)[0].replace(/\s/g, '');
-                    },
+                parseMemoryVolume: function(){
+                    return productNameNode.innerHTML.match(/(?:\s)\d+(\s+)?GB(?:\s)/g)[0].replace(/\s/g, '');
+                },
 
-                    buildProductData: function(){
-                        var productObject = {};
-                        productObject.ua_price = this.parseUAPrices();
-                        productObject.usd_price = this.parseUSDPrices();
-                        productObject.name = this.parseProductNames();
-                        productObject.url = this.parseProductURLS();
-                        productObject.memory = this.parseMemoryVolume();
+                buildProductData: function(){
+                    var productObject = {};
+                    productObject.ua_price = this.parseUAPrices();
+                    productObject.usd_price = this.parseUSDPrices();
+                    productObject.name = this.parseProductNames();
+                    productObject.url = this.parseProductURLS();
+                    productObject.memory = this.parseMemoryVolume();
 
-                        return productObject;
-                    }
+                    return productObject;
                 }
-            };
-
-            for(var i = 0; i < containers.length; i++){
-                products.push(parseContainerData(containers[i]).buildProductData());
             }
+        };
+
+        for(var i = 0; i < containers.length; i++){
+            products.push(parseContainerData(containers[i]).buildProductData());
         }
 
         return products;
@@ -82,50 +78,21 @@
             if (request.status === 200) {
                 var body = document.createElement('body');
                 body.innerHTML = request.responseText.match(/<body>[\s\S]*<\/body>/gim)[0];
-                parseData(body);
+                callback(parseData(body));
                 var pager = body.querySelectorAll('.goods-pages-list li');
                 totalPages = parseInt(pager[pager.length - 1].getAttribute('id').match(/\d+/g)[0]);
-                loadAllGoods();
             }
         }
     };
 
     request.send(null);
-
-    var loadAllGoods = function () {
-        //если текущая страница меньше общего числа страниц
-        if(currentPage < totalPages){
-            currentPage += 1;
-        } else {
-            return;
-        }
-
-        var currentPageURL = url + 'page=' + currentPage + '/';
-        request.open('GET', currentPageURL, true);
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    var body = document.createElement('body');
-                    body.innerHTML = request.responseText.match(/<body>[\s\S]*<\/body>/g)[0];
-                    parseData(body);
-                    if(currentPage === totalPages){
-                        callback(parseData(body));
-                    }
-                    /*parseData(body);*/
-                    //если наша страница последняя страница
-                    /*if (currentPage === totalPages) {
-                        callback(products);
-                    }*/
-                    loadAllGoods();
-                }
-            }
-        };
-
-        request.send();
-    };
 })(showData);
 
 function showData(data){
    console.log(data);
     console.log(data.length);
 }
+
+window.onerror = function(error){
+    console.log(error);
+};
