85825c5220750bb726153254b8843303632c8ab0
Viacheslav Okolitiy <justforgit@gmail.com>
2013-09-29 02:37:05 +0300

    Ready for testing

--- jquery-intro/sample.jquery.js
+++ jquery-intro/sample.jquery.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
+    //Убрать блок рекламы, если есть (в правой колонке)
     var removeAd = function(){
         $("div[class^='banner']").each(function(){
             if(this.className.match(/[0-9]/)){
@@ -11,6 +12,7 @@ $(document).ready(function(){
 
     removeAd();
 
+    //Убрать хабранавигатор (если есть)
     var removeHabraNavigator = function(){
         if($(".fast_navigator")){
             $(".fast_navigator").remove();
@@ -19,6 +21,8 @@ $(document).ready(function(){
 
     removeHabraNavigator();
 
+    //Разделить прямой эфир на 2 отдельных блока (посты, qa) (внутри есть переключалка).
+    // Блоки должны сохранить оригинальную стилистику
     var makeBlocks = function(){
         //находим элемент по классу live_broadcast
         var broadCast = $('.live_broadcast');
@@ -39,6 +43,7 @@ $(document).ready(function(){
 
     makeBlocks();
 
+    //Все блоки сделать collapsible
     var makeCollapsible = function(){
         $('.sidebar_right div .title').each(function() {
             //скрываем элементы указанные в селекторе
@@ -58,6 +63,7 @@ $(document).ready(function(){
 
     makeCollapsible();
 
+    //Убрать рейтинги, описание, флаги. Должны остаться только ссылки на пост и теги
     var hideMiscellaneous = function(){
         $('.infopanel_wrapper').hide();
         $('.flag').hide();
@@ -67,6 +73,7 @@ $(document).ready(function(){
 
     hideMiscellaneous();
 
+    //Справа от каждой ссылки поста показать количество комментариев к посту
     var showCommentsCount = function(){
         var comments = $('body').find('.comments .all');
         var posts = $('body').find('.posts_list .title');
@@ -76,4 +83,36 @@ $(document).ready(function(){
     };
 
     showCommentsCount();
+
+    //Подтянуть на страницу все посты в том-же формате с последующих страниц
+    var loadData = (function(){
+        var pages = parseInt($('#nav-pages > li').children().last().text());
+        var lastUrl = $('#nav-pages > li').children('li:last-child a').get(0).href;
+        var currentPage = 2;
+
+        return {
+            handleResponse: function(){
+                var currentUrl = lastUrl.replace(/\d+/g, currentPage);
+                currentPage++;
+                $.get(currentUrl, function(response){
+                    var element = document.createElement('div');
+                    element.innerHTML = response.match(/<body>[\s\S]*<\/body>/gim)[0];
+                    $(element).find('.infopanel_wrapper').hide();
+                    $(element).find('.flag').hide();
+                    $(element).find('.content.html_format').hide();
+                    $(element).find('.published').hide();
+
+                    $(element).find('.posts.shortcuts_items > div').appendTo($('.posts.shortcuts_items'));
+
+                    if(currentPage <= pages){
+                        loadData.handleResponse();
+                    } else {
+                        $('.page-nav').remove();
+                    }
+                });
+            }
+        }
+    }());
+
+    loadData.handleResponse();
 });
